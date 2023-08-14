7e34179f6591f84449d03922e5500675a87260de
jordi1q77 <jordi1q77@gmail.com>
2023-08-14 17:44:36 +0200

    create variables session 01

---
+++ 01-variables.js
@@ -0,0 +1,23 @@
+var greetings = 'hola desde';
+var school = 'pmba';
+
+var greetingsFromSchool = greetings+' '+school;
+console.log(greetingsFromSchool);
+
+
+//FINAL_TEXT(PASCAL_CASE)--> para constantes
+//holaQueTal(camelCase)
+
+// String
+// Number
+// Boolean
+// Array
+// Object
+
+var age = 30;
+var surprised = true;
+var arrayOfValues = [30, '30',false,true,4];
+var objetctOfValues = {
+    age:30,
+    surprised : surprised
+} 
