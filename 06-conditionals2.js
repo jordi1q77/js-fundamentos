a559df5454c22210dc39427a4df5dd11c2cfcff4
jordi1q77 <jordi1q77@gmail.com>
2023-08-18 11:49:01 +0200

    switch class

---
+++ 06-conditionals2.js
@@ -0,0 +1,20 @@
+var price;
+var city = "Valencia";
+
+switch (city) {
+  case "Valencia":
+    price = 10;
+    break;
+  case "Madrid":
+    price = 15;
+    break;
+  case "Barcelona":
+    price = 12;
+    break;
+  default:
+    price = 5;
+    break;
+}
+
+
+//switch para casos simples pq si no el código no queda legible
